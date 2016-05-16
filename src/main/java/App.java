b916e7170f8c4a5d1a4a330ccaf56ff94a6a4bc2
arlen <burton.arlen.1990@gmail.com>
2016-05-16 08:01:06 -0700

    building front end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,6 +14,48 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    
+    get("/venues", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/venues.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/bands", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/bands.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/venue-form", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/Venue-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/band-form", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/Band-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/venues", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("venue_name");
+      Venue newVenue = new Venue(name);
+      newVenue.save();
+      response.redirect("/venues");
+      return null;
+    });
+
+    post("/bands", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("band_name");
+      Band newBand = new Band(name);
+      newBand.save();
+      response.redirect("/bands");
+      return null;
+    });
+
+
   }
 }
