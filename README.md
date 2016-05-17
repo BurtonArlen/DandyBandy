83c83635efa9896fcc1976674b2cf895032025a5
arlen <burton.arlen.1990@gmail.com>
2016-05-17 07:17:14 -0700

    display dropdown for addBand or addVenue, will not properly add to either.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Project: HandyDandyBandyTracker
+
+#### _It creates a website where the user can input bands and venues, link them together, and then modify them. version 1.0, current as of 5.17.2016._
+
+#### By _** Arlen Burton **_
+
+## Description
+
+This is the fourth solo project from Epicodus' Java class. It creates a website where the user can select a stylist from a hair salon.
+
+The code and records for this open source project can be found at
+
+## Support and contact details
+
+Questions? Concerns? Ideas? Email: TBD
+
+## Technologies Used
+
+The program uses CSS, Bootstrap, Spark, Velocity, FluentLenium, Gradle, sql, and JAVA.
+
+### License
+
+*Released under the MIT license.*
+
+Copyright (c) 2016 **_Arlen Burton_**
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,12 +16,14 @@ public class App {
 
     get("/venues", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("venues", Venue.all());
       model.put("template", "templates/venues.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/bands", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("bands", Band.all());
       model.put("template", "templates/bands.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -47,6 +49,17 @@ public class App {
       return null;
     });
 
+    post("/venues-addband", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("venue_name");
+      Venue newVenue = new Venue(name);
+      int band_id = Integer.parseInt(request.queryParams("band_id"));
+      Band band = Band.find(band_id);
+      newVenue.addBand(band);
+      response.redirect("/venues");
+      return null;
+    });
+
     post("/bands", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       String name = request.queryParams("band_name");
@@ -55,6 +68,40 @@ public class App {
       response.redirect("/bands");
       return null;
     });
+
+    post("/bands-addvenue", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("band_name");
+      Band newBand = new Band(name);
+      int venue_id = Integer.parseInt(request.queryParams("venue_id"));
+      Venue venue = Venue.find(venue_id);
+      newBand.addVenue(venue);
+      response.redirect("/bands");
+      return null;
+    });
+
+    get("/bands/:id", (request,response) ->{
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Band band = Band.find(Integer.parseInt(request.params("id")));
+      model.put("band", band);
+      model.put("bands", Band.all());
+      model.put("venues_bands", band.getVenues());
+      model.put("venues", Venue.all());
+      model.put("template", "templates/band.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/venues/:id", (request,response) ->{
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Venue venue = Venue.find(Integer.parseInt(request.params("id")));
+      model.put("venue", venue);
+      model.put("venues", Venue.all());
+      model.put("venues_bands", venue.getBands());
+      model.put("bands", Band.all());
+      model.put("template", "templates/venue.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 
 
   }
